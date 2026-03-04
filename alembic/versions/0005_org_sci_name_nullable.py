cb875bb889b592ad13064b4ec10e446eaee1052d
Emily Marshall <emily.lucinda.m@gmail.com>
2026-03-04 19:35:44 +1100

    fix: shorten migration name to meet alembic constraint of 32 chars

--- alembic/versions/0005_org_sci_name_nullable.py
+++ alembic/versions/0005_org_sci_name_nullable.py
@@ -1,6 +1,6 @@
 """Make organism.scientific_name nullable.
 
-Revision ID: 0005_make_organism_scientific_name_nullable
+Revision ID: 0005_org_sci_name_nullable
 Revises: 0004_add_assembly_run
 Create Date: 2026-03-04 00:00:00.000000
 """
@@ -8,7 +8,7 @@ Create Date: 2026-03-04 00:00:00.000000
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "0005_make_organism_scientific_name_nullable"
+revision = "0005_org_sci_name_nullable"
 down_revision = "0004_add_assembly_run"
 branch_labels = None
 depends_on = None
