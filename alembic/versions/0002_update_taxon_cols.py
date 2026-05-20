f0b2fb1bb148666dbc5123d38554d018fbff8e0c
Emily Marshall <emily.lucinda.m@gmail.com>
2026-05-20 19:54:07 +1000

    refactor: merge migrations

--- alembic/versions/0002_update_taxon_cols.py
+++ alembic/versions/0002_update_taxon_cols.py
@@ -4,10 +4,13 @@ Revision ID: 0002_update_taxon_cols
 Revises: 0001_initial_schema
 Create Date: 2026-05-11
 
-Removing columns "defined_class" (redundant with "ncbi_class"), "mito_ref" (redundant with "mitohifi_reference_species"), and "busco_dataset_name" (replaced by "busco_odb10_dataset_name" and "busco_odb12_dataset_name").
+Removing columns "defined_class" (redundant with "ncbi_class"), "mito_ref" (redundant with "mitohifi_reference_species"), and
+"busco_dataset_name" (replaced by "busco_odb10_dataset_name" and "busco_odb12_dataset_name"). Also adding "hic_specimen_sample_ids"
+column to assembly table, to replace "hic_specimen_sample_id".
 """
 
 import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import JSONB
 
 from alembic import op
 
@@ -30,6 +33,7 @@ def upgrade() -> None:
     )
     op.drop_column("taxonomy_info", "mito_ref")
     op.drop_column("taxonomy_info", "busco_dataset_name")
+    op.add_column("assembly", sa.Column("hic_specimen_sample_ids", JSONB, nullable=True))
 
 
 def downgrade() -> None:
@@ -45,3 +49,4 @@ def downgrade() -> None:
         "taxonomy_info",
         sa.Column("busco_dataset_name", sa.Text(), nullable=True),
     )
+    op.drop_column("assembly", "hic_specimen_sample_ids")
